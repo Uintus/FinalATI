1336bb09ebde64729dd7e727716a48a21571d031
Sew Chan <sew9196@gmail.com>
2024-11-13 21:15:34 +0700

    finish demo

--- FinalATI/app.py
+++ FinalATI/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -10,6 +9,10 @@ def home():
 @app.route('/detail')
 def detail():
     return render_template('detail.html')
+
+@app.route('/grading')
+def grading():  # Changed the function name from detail to grading
+    return render_template('grading.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
