e92ba243645737b90e1a7f5441adcd33f16ba9d6
UinTus <tuuyen294@gmail.com>
2024-11-14 21:09:29 +0700

    add delete exam

--- FinalATI/static/utility/homePage.js
+++ FinalATI/static/utility/homePage.js
@@ -4,6 +4,7 @@ const exitFormBtn = document.querySelector('.dlt_icon')
 const formBox = document.querySelector('.addCourse__form')
 const blurLayer = document.querySelector('.hidden_layer')
 
+
 addCourseBtn.addEventListener('click', handleShowForm)
 exitFormBtn.addEventListener('click', handleHiddenForm)
 
@@ -18,3 +19,45 @@ function handleHiddenForm() {
 }
 
 //_________________________HANDLE POST FORM__________________________
+
+
+
+
+//_________________________HANDLE DELETE EXAM_______________________
+const dotsBtns = document.querySelectorAll('.course--dots');
+const dltBoxes = document.querySelectorAll('.course--delete');
+const dltPopup = document.querySelector('.popup--dlt'); 
+const dltBtn = document.querySelector('.popup--dlt button');
+const exitPopup = document.querySelector('.popup--dlt i');
+
+dotsBtns.forEach((dotsBtn, index) => {
+  dotsBtn.addEventListener('mouseenter', () => handleShowDltBox(index));
+  dotsBtn.addEventListener('mouseleave', () => handleShowDltBox(index));
+});
+
+dltBoxes.forEach((dltBox) => {
+  dltBox.addEventListener('click', handleDltPopup);
+});
+
+exitPopup.addEventListener('click', handleHiddenPopup);
+dltBtn.addEventListener('click', handleDeleteExam);
+
+function handleShowDltBox(index) {
+  dltBoxes[index].classList.toggle('hidden');
+}
+
+function handleDltPopup() {
+  blurLayer.classList.add('blur');
+  dltPopup.style.top = '200px';
+}
+
+function handleHiddenPopup() {
+  blurLayer.classList.remove('blur');
+  dltPopup.style.top = '';
+}
+
+// delete exam here in DB
+function handleDeleteExam() {
+  blurLayer.classList.remove('blur');
+  dltPopup.style.top = '';
+}
