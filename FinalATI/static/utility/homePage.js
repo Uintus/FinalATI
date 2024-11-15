b3587520253ae28966582d46cc45fd484a1253eb
UinTus <tuuyen294@gmail.com>
2024-11-15 09:50:41 +0700

    update home

--- FinalATI/static/utility/homePage.js
+++ FinalATI/static/utility/homePage.js
@@ -1,63 +1,69 @@
 //_________________________HANDLE SHOW/HIDDEN FORM__________________________
-const addCourseBtn = document.querySelector('.detail--left__addCourse')
-const exitFormBtn = document.querySelector('.dlt_icon')
-const formBox = document.querySelector('.addCourse__form')
-const blurLayer = document.querySelector('.hidden_layer')
+const addCourseBtn = document.querySelector(".detail--left__addCourse");
+const exitFormBtn = document.querySelector(".dlt_icon");
+const formBox = document.querySelector(".addCourse__form");
+const blurLayer = document.querySelector(".hidden_layer");
 
-
-addCourseBtn.addEventListener('click', handleShowForm)
-exitFormBtn.addEventListener('click', handleHiddenForm)
+addCourseBtn.addEventListener("click", handleShowForm);
+exitFormBtn.addEventListener("click", handleHiddenForm);
 
 function handleShowForm() {
-    formBox.style.top = '37px'
-    blurLayer.classList.add('blur')
+  formBox.style.top = "37px";
+  blurLayer.classList.add("blur");
 }
 
 function handleHiddenForm() {
-    formBox.style.top = ''
-    blurLayer.classList.remove('blur')
+  formBox.style.top = "";
+  blurLayer.classList.remove("blur");
 }
 
 //_________________________HANDLE POST FORM__________________________
 
-
-
-
 //_________________________HANDLE DELETE EXAM_______________________
-const dotsBtns = document.querySelectorAll('.course--dots');
-const dltBoxes = document.querySelectorAll('.course--delete');
-const dltPopup = document.querySelector('.popup--dlt'); 
-const dltBtn = document.querySelector('.popup--dlt button');
-const exitPopup = document.querySelector('.popup--dlt i');
+const dotsBtns = document.querySelectorAll(".course--dots");
+const dltBoxes = document.querySelectorAll(".course--delete");
+const dltPopup = document.querySelector(".popup--dlt");
+const dltBtn = document.querySelector(".popup--dlt button");
+const exitPopup = document.querySelector(".popup--dlt i");
 
 dotsBtns.forEach((dotsBtn, index) => {
-  dotsBtn.addEventListener('mouseenter', () => handleShowDltBox(index));
-  dotsBtn.addEventListener('mouseleave', () => handleShowDltBox(index));
+  dotsBtn.addEventListener("mouseenter", () => handleShowDltBox(index));
+  dotsBtn.addEventListener("mouseleave", () => handleShowDltBox(index));
 });
 
-dltBoxes.forEach((dltBox) => {
-  dltBox.addEventListener('click', handleDltPopup);
+
+//  send được index của exam vào handleDeleteExam
+dltBoxes.forEach((dltBox, index) => {
+  dltBox.addEventListener("click", function () {
+    handleDltPopup();
+    const dltBtnClickListener = () => {
+      handleDeleteExam(index);
+      dltBtn.removeEventListener("click", dltBtnClickListener);
+    };
+    dltBtn.addEventListener("click", dltBtnClickListener);
+  });
 });
 
-exitPopup.addEventListener('click', handleHiddenPopup);
-dltBtn.addEventListener('click', handleDeleteExam);
+exitPopup.addEventListener("click", handleHiddenPopup);
 
 function handleShowDltBox(index) {
-  dltBoxes[index].classList.toggle('hidden');
+  dltBoxes[index].classList.toggle("hidden");
 }
 
 function handleDltPopup() {
-  blurLayer.classList.add('blur');
-  dltPopup.style.top = '200px';
+  blurLayer.classList.add("blur");
+  dltPopup.style.top = "200px";
 }
 
 function handleHiddenPopup() {
-  blurLayer.classList.remove('blur');
-  dltPopup.style.top = '';
+  blurLayer.classList.remove("blur");
+  dltPopup.style.top = "";
 }
 
 // delete exam here in DB
-function handleDeleteExam() {
-  blurLayer.classList.remove('blur');
-  dltPopup.style.top = '';
+function handleDeleteExam(index) {
+  blurLayer.classList.remove("blur");
+  dltPopup.style.top = "";
+
+  console.log("the exam at: " + index + " button");
 }
